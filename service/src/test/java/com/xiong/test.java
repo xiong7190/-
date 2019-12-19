eabc586f9e4f64acb3c35023a6d11eb9e9b37ecf
xiong <xiong.fox@foxmail.com>
2019-12-20 00:54:01 +0800

    旅游后台管理系统

--- service/src/test/java/com/xiong/test.java
+++ service/src/test/java/com/xiong/test.java
@@ -1,10 +1,5 @@
 package com.xiong;
 
-import com.xiong.service.UserService;
-import com.xiong.service.impl.UserServiceImpl;
-import org.junit.Test;
-
-import java.sql.SQLOutput;
 
 
 public class test {
